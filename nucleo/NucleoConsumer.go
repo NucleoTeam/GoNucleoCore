7bea24ba0837a22b01601edafcd9dbe5ae0ab1a2
Nathaniel <nathaniel.davidson@gmail.com>
2019-06-23 11:25:52 -0700

    changed to confluence client

--- nucleo/NucleoConsumer.go
+++ nucleo/NucleoConsumer.go
@@ -1,51 +1,49 @@
 package nucleohub
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/segmentio/kafka-go"
-	"time"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 type NucleoConsumer struct {
 	Brokers []string
 	Chain string
 	GroupID string
-	Reader *kafka.Reader
+	Consumer *kafka.Consumer
 	Hub *NucleoHub
 }
 
 func NewConsumer(chain string, group string, brokers []string, hub *NucleoHub) *NucleoConsumer {
-	fmt.Println("Registered chain: " + chain)
 	consumer := new(NucleoConsumer);
 	consumer.Brokers = brokers
 	consumer.GroupID = group
 	consumer.Hub = hub
 	consumer.Chain = chain
 	c, _ := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "localhost",
-		"group.id":          "myGroup",
+		"bootstrap.servers": brokers[0],
+		"group.id":          consumer.GroupID,
 		"auto.offset.reset": "earliest",
 	})
+	consumer.Consumer = c
+	consumer.Consumer.Subscribe(chain, nil)
+
 	go consumer.readThread()
 
 	return consumer;
 }
 func (c * NucleoConsumer) readThread(){
-		for {
-			m, err := c.Reader.FetchMessage(context.Background())
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-			data := NewNucleoData()
-			err = json.Unmarshal([]byte(m.Value), &data)
-			if err != nil {
-				fmt.Println(err)
-			}
-			if data.Objects == nil {
-				data.Objects = map[string]interface{}{}
-			}
-			c.Hub.Execute(m.Topic, data)
+	for {
+		m, err := c.Consumer.ReadMessage(-1)
+		if err != nil {
+			fmt.Println(err)
+			break
 		}
+		data := NewNucleoData()
+		err = json.Unmarshal(m.Value, &data)
+		if err != nil {
+			fmt.Println(err)
+		}
+		c.Hub.Execute(m.TopicPartition.String(), data)
+	}
 }
